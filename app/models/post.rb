d5298f2700cb96a64a6795931769a87479965f38
Manuel Grullon <astermanuelg@gmail.com>
2018-04-10 17:26:48 -0400

    Done.

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,19 @@
 class Post < ActiveRecord::Base
+  validates :title, presence: true
+  validates :content, length: {minimum: 250}
+  validates :summary, length: {maximum: 250}
+  validates :category, inclusion: { in: %W(Fiction Non-Fiction)}
+ validate :is_clickbait?
+
+
+#this is the worst ruby code I've ever written. I'm sorry. I'm so sorry.
+  def is_clickbait?
+    if self.title
+      clickbait_titles = ["Won't Believe", "Secret", "Top", "Guess"]
+      if !clickbait_titles.any? {|cb| self.title.include?(cb)}
+        errors.add(:title, "no click bait")
+      end
+    end
+  end
+
 end
