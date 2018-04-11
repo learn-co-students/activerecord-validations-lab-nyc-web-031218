06645ee269d859a6b732eb38f0bcaf336bcb78e0
harim1206 <harim1206@gmail.com>
2018-04-11 12:55:50 -0400

    Done.

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,23 @@
 class Post < ActiveRecord::Base
+  validates :title, presence: true
+  validates :content, length: {minimum: 250}
+  validates :summary, length: {maximum: 250}
+  validates :category, inclusion: ["Fiction", "Non-fiction"]
+
 end
+
+
+# All posts have a title
+# Post content is at least 250 characters long
+# Post summary is a maximum of 250 characters
+
+# t.string   "title"
+# t.text     "content"
+# t.text     "summary"
+# t.string   "category"
+
+#   is invalid without a title (FAILED - 1)
+#   is invalid with too short content (FAILED - 2)
+#   is invalid with a long summary (FAILED - 3)
+#   is invalid with a category outside the choices (FAILED - 4)
+#   is invalid if not clickbait (FAILED - 5)
